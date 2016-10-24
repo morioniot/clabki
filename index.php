41449e5c797d9b87bc01b0e8f4e5ad4a29b9edf0
Miguel Ramos <miguel9ramos@gmail.com>
2016-10-24 12:16:25 -0500

    Agragando el index de la página

---
+++ index.php
@@ -0,0 +1,10 @@
+.<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Clabki</title>
+    </head>
+    <body>
+        <?php echo("Welcome to Clabki!!!"); ?>
+    </body>
+</html>
