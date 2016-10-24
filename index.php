fd501a8d607f7d3c39ec8eb37759c3431840cac9
Nicolás Fernández <fercholas2012@gmail.com>
2016-10-24 17:06:18 -0500

    Header e imagen principal

--- index.php
+++ index.php
@@ -1,12 +1,42 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>Clabki</title>
-    </head>
-    <body>
-        <?php echo("Welcome to Clabki!!!"); ?>
-        <?php echo("Working with Clabki in OpenShift!!!"); ?>
-        <?php echo("Working with Clabki in OpenShift Oh yeah! !!!!"); ?>
-    </body>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<link rel="stylesheet" href="./Public/stylesheets/style.css" type="text/css" media="all">
+	<title> Clabki | La mejor manera de encontrar a tu mascota perdida.</title>
+</head>
+<body>
+	<header class="header">
+		<div class="logo">
+			<a href="http://wordpress-morion.rhcloud.com/" >
+				<img src="./Public/img/logoLarge.png" alt="Clabki">
+			</a>
+			<p>Deja una huella siguiendo huellas</p>
+		</div>
+		<nav class="social_networks">
+			<ul>
+				<li>
+					<a href="#">
+						<img src="./Public/img/facebook.png" alt="Clabki">
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<img src="./Public/img/twitter.png" alt="Clabki">
+					</a>
+				</li>
+			</ul>			
+		</nav>				
+		<nav class="navigation">
+			<ul >
+				<li >
+					<a href="#">¿Quiénes somos?</a>
+				</li>
+			</ul>			
+		</nav>
+	</header> 
+	<div id="main_image">
+		
+	</div>
+</body>
 </html>
