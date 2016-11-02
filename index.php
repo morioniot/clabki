31d0b5e951443b220c3e9df3c0a75594f16dba12
Nicolás Fernández <fercholas2012@gmail.com>
2016-11-02 17:09:37 -0500

    Estructura sección ¿De que se trata? y responsive parcial

--- index.php
+++ index.php
@@ -10,10 +10,10 @@
 <body>
 	<header class="header">
 		<div class="logo">
-			<a href="http://wordpress-morion.rhcloud.com/" >
+			<a href="#" >
 				<img src="./Public/img/logoLarge.png" alt="Clabki">
 			</a>
-			<p id"lema_line">DEJA TU HUELLA SIGUIENDO HUELLAS</p>
+			<p>DEJA TU HUELLA SIGUIENDO HUELLAS</p>
 		</div>
 		<nav class="social_networks" >
 			<ul id="boton_siguenos">
@@ -39,5 +39,89 @@
 			<img id="deQueSeTrataButton"src="./Public/img/btn_dequesetrata.png" alt="¿De qué se trata?">
 		</div>
 	</div>
+	<div id="content">
+		<section class="section" id="section_DeQueSeTrata"> 
+			<div class="container">
+				<div class="section-header">
+					<h2>¿DE QUÉ SE TRATA?</h2>
+				</div>
+				<div class="sub_section"> 
+					<h3> COMUNIDAD </h3>
+					<div class="text_description"> 
+						<p>
+							En Clabki creemos que podemos hacer mucho más por las mascotas perdidas. Por eso buscamos crear la comunidad más grande de personas con una idea en común: “No queremos más mascotas lejos de su hogar“.
+							<br>
+							<br>
+							Gracias a una aplicación móvil, cada miembro de la comunidad se convertirá en un radar que vigilará la ciudad en busca de mascotas Clabki que hayan sido reportadas como perdidas.
+						</p>
+					</div>
+					<div class="img_description">
+					</div>
+				</div>
+				<div class="sub_section"> 
+					<h3> UNA MASCOTA CLABKI </h3>
+					<div class="text_description"> 
+						<p>
+							Una mascota Clabki es una mascota que tiene un dispositivo inteligente en su collar que emite constantemente un código con el cual puede ser reconocida por los miembros de la comunidad que se encuentren cerca de ella.
+						</p>
+					</div>
+					<div class="img_description">
+					</div>
+				</div>
+				<div class="sub_section"> 
+					<h3> EN CASO DE PÉRDIDA </h3>
+					<div class="text_description"> 
+						<p>
+							En caso de que una mascota Clabki se pierda, su dueño puede reportarla a través de la misma aplicación para que el radar formado por nuestra comunidad pueda detectarla.
+							<br>
+							<br>
+							A partir de ese momento, cualquier miembro de nuestra comunidad que pase cerca de la mascota le reportará automáticamente a su dueño la posición en la cual se generó el encuentro. Diferentes encuentros con los miembros de la comunidad le informarán al dueño los lugares por los cuales se está moviendo su mascota.
+							<br>
+							<br>
+							Una vez la mascota esté en su hogar de nuevo te avisaremos que has sido parte fundamental de un final feliz. Al final del día todos habremos ganado.
+						</p>
+					</div>
+					<div class="img_description">
+					</div>
+				</div>
+				<div class="sub_section"> 
+					<h3> !HAZ PARTE! </h3>
+					<div class="text_description"> 
+						<p>
+							Anímate a ser parte del radar más grande de mascotas perdidas. Lo único que tendrás que hacer es descargar una aplicación gratuita en tu celular. Con ella podrás buscar mascotas perdidas mientras corres, haces bici, vas al trabajo, etc. Entre más seamos, más efectiva será nuestra búsqueda y menos mascotas en las calles habrá.
+						</p>
+					</div>
+					<div class="img_description">
+					</div>
+				</div>
+				<div id="cta_button_container">
+					<img src="./Public/img/btn_dequesetrata.png" alt="¿Haz parte?">
+				</div>
+			</div>
+		</section>
+	</div>
+	<footer>
+		<div class="container">
+			<div class="footer_item">
+				<div class="footer_icon_container">
+					<img src="http://wordpress-morion.rhcloud.com/wp-content/themes/zerif-lite/images/map25-redish.png">
+				</div>
+				<p> Calle 24 A # 59 59 </p>
+			</div>
+			<div class="footer_item">
+				<div class="footer_icon_container">
+					<img src="http://wordpress-morion.rhcloud.com/wp-content/themes/zerif-lite/images/envelope4-green.png">
+				</div>
+				<p> morion.iot@gmail.com </p>
+			</div>
+			<div class="footer_item">
+				<div class="footer_icon_container">
+					<img src="http://wordpress-morion.rhcloud.com/wp-content/themes/zerif-lite/images/telephone65-blue.png">
+				</div>
+				<p> 3108529139 </p>
+			</div>
+
+		</div>
+	</footer>
 </body>
 </html>
