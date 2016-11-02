b331f492f481a4b71fa53a1a988b69a9c2629483
Nicolás Fernández <fercholas2012@gmail.com>
2016-11-02 17:53:55 -0500

    Cambio media max-width

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=0">
 	<link rel="stylesheet" href="./Public/stylesheets/style.css" type="text/css" media="all">
 	<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
 	<script type="text/javascript" src="./Public/js/main.js"></script>
