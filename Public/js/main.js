a9df0fd08fdd4027ca1ead2ac996a53a4f15c6af
Nicolás Fernández <fercholas2012@gmail.com>
2016-11-21 12:17:02 -0500

    Página principal ajustes de tamaños

--- Public/js/main.js
+++ Public/js/main.js
@@ -1,48 +1,6 @@
 $( document ).ready(function() {
 
-   //** Handler 'Síguenos' botton
 
-    $('#boton_siguenos').hover(
-        function(){
-            $('#siguenos_img').attr("src","./Public/img/btn_siguenos_activo.png");
-            $('#boton_siguenos').css("bottom","-100%");
-        },
-        function(){ 
-            $('#siguenos_img').attr("src","./Public/img/btn_siguenos.png");
-            $('#boton_siguenos').css("bottom","0%");
-        }
-    );
-
-   //** ** ** ** ** ** *** *** * 
-
-   //** Handler 'Conócenos' botton
-
-    $('#boton_conocenos').hover(
-        function(){
-            $('#conocenos_img').attr("src","./Public/img/btn_conocenos_activo.png");
-            $('#boton_conocenos').css("bottom","-100%");
-        },
-        function(){ 
-            $('#conocenos_img').attr("src","./Public/img/btn_conocenos.png");
-            $('#boton_conocenos').css("bottom","0%");
-        }
-    );
-
-   //** ** ** ** ** ** *** *** *    
-
-   //** Handler 'deQuéSeTrata' botton
-
-    $('.buttons').hover(
-        function(){
-            $('#deQueSeTrataButton').attr("src","./Public/img/btn_dequesetrata_activo.png");
-        },
-        function(){ 
-            $('#deQueSeTrataButton').attr("src","./Public/img/btn_dequesetrata.png");
-
-        }
-    );
-
-   //** ** ** ** ** ** *** *** * 
 
 });
 
--- index.php
+++ index.php
@@ -3,215 +3,167 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=0" />
-	<link href="https://fonts.googleapis.com/css?family=Concert+One|Source+Sans+Pro" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Concert+One|Source+Sans+Pro:400,700" rel="stylesheet">
 	<link rel="stylesheet" href="./Public/stylesheets/style.css" type="text/css" media="all">
 	<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
-	<script type="text/javascript" src="./Public/js/main.js"></script>
 	<title> Clabki | La mejor manera de encontrar a tu mascota perdida.</title>
 </head>
 <body>
 	<div id="wrapper">
-		
-		<header class="header">
-			<div class="logo">
-				<a href="#" >
-					<img src="./Public/img/logo.png" alt="Clabki">
-				</a>
-			</div>
-			<nav class="social_networks" >
-				<ul id="boton_siguenos">
-					<li>
-						<a href="#">
-							<img id="siguenos_img" src="./Public/img/btn_siguenos.png" alt="Síguenos">
-						</a>
-					</li>
-				</ul>			
-			</nav>				
-			<nav class="navigation">
-				<ul id="boton_conocenos">
-					<li >
-						<a href="#">
-							<img id="conocenos_img" src="./Public/img/btn_conocenos.png" alt="Conócenos">
-						</a>
-					</li>
-				</ul>			
-			</nav>
-		</header> 
-		<div id="main_image">
-				<img src="./Public/img/background_V2.jpg" alt="">
-		</div>
-		<div id="content">
-			<section class="section" id="section_DeQueSeTrata"> 
-				<div class="container">
-					<div class="section-header">
-						<p class="title">¿DE QUÉ SE TRATA?</p>
-					</div>
-					<div  id="comunidad_sub_section" class="sub_section"> 
-						<section id="comunidad_sub_section_container" class="sub_section_container"> 
-							<div class="text_description"> 
-								<div class="text_container"> 
-									<p class="title"> COMUNIDAD </p>
-									<hr class="separator"> 
-									<div class= "text_content">
-										<p class="simple_paragraph"> En <span> Clabki </span> creemos que podemos hacer mucho más por las mascotas perdidas. Por eso buscamos crear la comunidad más grande de personas con una idea en común: </p>
-										<p class="important_text"> “No queremos más mascotas lejos de su hogar“ </p>
-										<p class="simple_paragraph"> Gracias a una aplicación móvil, cada miembro de la comunidad se convertirá en un radar que vigilará la ciudad en busca de mascotas Clabki que hayan sido reportadas como perdidas. </p>
-									</div>
+        <header>
+            <div id="logo_container">
+                <a href="#">
+                    <img src="./Public/img/joinus/logo.png" alt="Clabki"/>
+                </a>
+            </div>
+            <nav id="contact_navigation">
+                <ul>
+                    <li>
+                        <a id="social_link" href="#">
+                            <!-- <img src="./Public/img/joinus/siguenos.png" alt="Redes"/> -->
+                        </a>
+                    </li>
+                    <li>
+                        <a id="contact_us_link" href="#">
+                            <!-- <img src="./Public/img/joinus/conocenos.png" alt="Conócenos"/> -->
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+		<section id="main_image">
+				<img src="./Public/img/main_image.jpg" alt="">
+		</section>
+		<section class="section" id="section_DeQueSeTrata"> 
+				<div id="pincel1_separator" class="section_separator"> 
+					<img src="./Public/img/pincel1.png">
+				</div>
+				<div class="section-header">
+					<img src="./Public/img/dequesetrata_titulo.png" alt="">
+				</div>
+				<div  id="comunidad_sub_section" class="sub_section"> 
+					<section id="comunidad_sub_section_container" class="sub_section_container"> 
+						<div class="text_description"> 
+							<div class="text_container"> 
+								<img src="./Public/img/comunidad.png" alt="">
+								<hr class="separator"> 
+								<div class= "text_content">
+									<p class="simple_paragraph"> En <span> Clabki </span> creemos que podemos hacer mucho más por las mascotas perdidas. Por eso buscamos crear la comunidad más grande de personas con una idea en común: </p>
+									<p class="important_text"> “No queremos más mascotas lejos de su hogar“ </p>
+									<hr id="special_separator" class="separator"> 
+									<p class="simple_paragraph"> Gracias a una aplicación móvil, cada miembro de la comunidad se convertirá en un radar que vigilará la ciudad en busca de mascotas Clabki que hayan sido reportadas como perdidas. </p>
 								</div>
 							</div>
-							<div class="img_description">
-								<img src="./Public/img/comunidad_icono.png">
-							</div>						
-						</section>
-					</div>
-					<div id="pincel1_separator" class="section_separator"> 
-						<img src="./Public/img/pincel1.png">
-					</div>
-					<div  id="mascota_sub_section" class="sub_section"> 
-						<section id="mascota_sub_section_container" class="sub_section_container"> 
-							<div class="img_description">
-								<img src="./Public/img/mascota_icono.png">
-							</div>						
-							<div class="text_description"> 
-								<div class="text_container"> 
-									<div>
-										<p class="title small"> UNA </p>  
-										<p class="title"> MASCOTA </p>
-										<p class="title"> <span>CLABKI</span> </p>  
-
-									</div>
-									<hr class="separator">
-									<div class= "text_content">
-										<p class="simple_paragraph"> Es una mascota que tiene un dispositivo inteligente en su collar que emite una señal con la cual puede ser reconocida por los miembros de la comunidad que se encuentren cerca de ella. </p>
+						</div>
+						<div id="comunidad_img_container" class="img_description">
+							<img src="./Public/img/comunidad_icono_17nov.png">
+						</div>						
+					</section>
+				</div>
+				<div id="pincel1_separator" class="section_separator"> 
+					<img src="./Public/img/pincel1.png">
+				</div>
+				<div  id="mascota_sub_section" class="sub_section"> 
+					<section id="mascota_sub_section_container" class="sub_section_container"> 
+						<div id="mascota_img_container" class="img_description">
+							<img src="./Public/img/mascota_icono.png">
+						</div>						
+						<div id="mascota_text_container" class="text_description"> 
+							<div class="text_container"> 
+								<img src="./Public/img/unamascota.png" alt="">
+								<hr class="separator">
+								<div class= "text_content">
+									<p class="simple_paragraph"> Es una mascota que tiene un dispositivo inteligente en su collar que emite una señal con la cual puede ser reconocida por los miembros de la comunidad que se encuentren cerca de ella. </p>
+								</div>
+							</div>
+						</div>						
+					</section>
+				</div>
+				<div id="pincel2_separator" class="section_separator">
+					<img src="./Public/img/pincel2.png">
+				</div>
+				<div  id="perdida_sub_section" class="sub_section"> 
+					<section id="perdida_sub_section_container" class="sub_section_container"> 
+						<div class="text_image_group"> 
+								<div class="text_description">
+									<div class="text_container"> 
+										<img src="./Public/img/quepasa.png" alt="">
+										<hr class="separator">
+										<div class= "text_content">
+											<p class="simple_paragraph"> En caso de que una mascota <span> Clabki </span> se pierda, su dueño puede reportarla a través de la misma aplicación para que el radar formado por nuestra comunidad pueda detectarla. </p> 
+										</div>
 									</div>
 								</div>
-							</div>						
-						</section>
-					</div>
-					<div id="pincel2_separator" class="section_separator">
-						<img src="./Public/img/pincel2.png">
-					</div>
-					<div  id="perdida_sub_section" class="sub_section"> 
-						<section id="perdida_sub_section_container" class="sub_section_container"> 
-							<div class="text_image_group"> 
-									<div class="text_description">
-										<div class="text_container"> 
-											<div>
-												<p class="title small">¿QUÉ PASA</p>  
-												<p class="title">EN CASO</p>
-												<p class="title"><span>DE PÉRDIDA?</span></p>  
-
-											</div>
-											<hr class="separator">
-											<div class= "text_content">
-												<p class="simple_paragraph"> En caso de que una mascota <span> Clabki </span> se pierda, su dueño puede reportarla a través de la misma aplicación para que el radar formado por nuestra comunidad pueda detectarla. </p> 
-											</div>
+								<div class="img_description">
+									<img id="celular_img" src="./Public/img/celular.png">
+								</div>
+						</div>
+						<div  class="text_image_group"> 
+								<div class="img_description">
+									<img id="mapa_img" src="./Public/img/mapa.png">
+								</div>
+								<hr class="separator">
+								<div class="text_description">
+									<div class="text_container"> 
+										<div class= "text_content"> 
+											<p class="simple_paragraph"> A partir de ese momento, cualquier miembro de <span> Clabki </span> que pase cerca de la mascota le reportará automáticamente a su dueño la ubicación en la cual se generó el encuentro. Diferentes encuentros le informarán al dueño los lugares por los cuales se está moviendo su mascota. </p>
 										</div>
-									</div>
-									<div class="img_description">
-										<img id="celular_img" src="./Public/img/celular.png">
-									</div>
-							</div>
-							<div class="text_image_group"> 
-									<div class="img_description">
-										<img id="mapa_img" src="./Public/img/mapa.png">
-									</div>
-									<hr id="special_separator" class="separator">
-									<div class="text_description">
-										<div class="text_container"> 
-											<div class= "text_content"> 
-												<p class="simple_paragraph"> A partir de ese momento, cualquier miembro de <span> Clabki </span> que pase cerca de la mascota le reportará automáticamente a su dueño la ubicación en la cual se generó el encuentro. Diferentes encuentros le informarán al dueño los lugares por los cuales se está moviendo su mascota. </p>
-											</div>
-										</div>
-									</div>
-							</div>												
-						</section>
-					</div>
-					<div id="pincel3_separator" class="section_separator">
-						<img src="./Public/img/pincel3.png">
-					</div>
-					<div  id="reencuentro_sub_section" class="sub_section"> 
-						<section id="reencuentro_sub_section_container" class="sub_section_container"> 
-							<div class="text_description"> 
-								<div class="text_container"> 
-											<div>
-												<p class="title small">¡FINAL</p>  
-												<p class="title"><span>FELIZ!</span></p>  
-
-											</div>
-									<hr class="separator">
-									<div class= "text_content">
-										<p class="simple_paragraph"> Una vez la mascota Clabki esté en su hogar de nuevo, te avisaremos que has sido parte fundamental del reencuentro de una familia.</p>
-										<p class="important_text">Al final del día ganamos todos</p>
 									</div>
 								</div>
-							</div>	
-							<div class="img_description">
+						</div>												
+					</section>
+				</div>
+				<div id="pincel3_separator" class="section_separator">
+					<img src="./Public/img/pincel3.png">
+				</div>
+				<div  id="reencuentro_sub_section" class="sub_section"> 
+					<section id="reencuentro_sub_section_container" class="sub_section_container"> 
+						<div id="final_feliz_text_container" class="text_description"> 
+							<div class="text_container"> 
+								<img src="./Public/img/finalfeliz.png" alt="">
+								<hr class="separator">
+								<div class= "text_content">
+									<p class="simple_paragraph"> Una vez la mascota Clabki esté en su hogar de nuevo, te avisaremos que has sido parte fundamental del reencuentro de una familia.</p>
+									<p class="important_text">Al final del día ganamos todos</p>
+								</div>
+							</div>
+						</div>	
+						<div id="final_feliz_img_container" class="img_description">
 
-							</div>						
-						</section>
-					</div>	
-					<div id="pincel4_separator" class="section_separator">
-						<img src="./Public/img/pincel4.png">
-					</div>		
-					<div  id="unete_sub_section" class="sub_section"> 
-						<section id="unete_sub_section_container" class="sub_section_container"> 
-							<div class="text_image_group"> 
-									<div class="text_description">
-										<div class="text_container"> 
-											<div>  
-												<p class="title">HAZ</p>
-												<p class="title"><span>PARTE</span></p>  
-
-											</div>
-											<hr class="separator">
-											<div class= "text_content">
-												<p class="simple_paragraph"> Anímate a ser parte <span> del radar más grande de mascotas perdidas. </span> Lo único que tendrás que hacer es descargar una aplicación gratuita en tu celular. Con ella podrás buscar mascotas perdidas mientras corres, montas bici, vas al trabajo, o realizas cualquier actividad de tu vida cotidiana. </p>
-											</div>
+						</div>						
+					</section>
+				</div>	
+				<div id="pincel4_separator" class="section_separator">
+					<img src="./Public/img/pincel4.png">
+				</div>		
+				<div  id="unete_sub_section" class="sub_section"> 
+					<section id="unete_sub_section_container" class="sub_section_container"> 
+						<div class="text_image_group"> 
+								<div class="text_description">
+									<div class="text_container"> 
+										<img src="./Public/img/hazparte.png" alt="">
+										<hr class="separator">
+										<div class= "text_content">
+											<p class="simple_paragraph"> Anímate a ser parte <span> del radar más grande de mascotas perdidas. </span> Lo único que tendrás que hacer es descargar una aplicación gratuita en tu celular. Con ella podrás buscar mascotas perdidas mientras corres, montas bici, vas al trabajo, o realizas cualquier actividad de tu vida cotidiana. </p>
 										</div>
 									</div>
-							</div>
-							<div class="text_image_group"> 
-									<div class="text_description">
-										<p class="text_container"> 
-											<div class= "text_content"> 
-												<p class="important_text">Entre más seamos, más efectiva será nuestra búsqueda y menor el número de mascotas lejos de su hogar.</p>
-											</div>
-										</p>
-									</div>
-									<div class="img_description">
-										<img id="quiero_img" src="./Public/img/quiero_btn.png">
-									</div>
-							</div>							
-						</section>
-					</div>
+								</div>
+						</div>
+						<div class="text_image_group"> 
+								<div class="text_description">
+									<p class="text_container"> 
+										<div class= "text_content"> 
+											<p class="important_text">Entre más seamos, más efectiva será nuestra búsqueda y menor el número de mascotas lejos de su hogar.</p>
+										</div>
+									</p>
+								</div>
+								<div class="img_description">
+									<img id="quiero_img" src="./Public/img/quiero_btn.png">
+								</div>
+						</div>							
+					</section>
 				</div>
-			</section>
-		</div>
-		<footer>
-			<div class="container">
-				<div class="footer_item">
-					<div class="footer_icon_container">
-						<img src="http://wordpress-morion.rhcloud.com/wp-content/themes/zerif-lite/images/map25-redish.png">
-					</div>
-					<p> Calle 24 A # 59 59 </p>
-				</div>
-				<div class="footer_item">
-					<div class="footer_icon_container">
-						<img src="http://wordpress-morion.rhcloud.com/wp-content/themes/zerif-lite/images/envelope4-green.png">
-					</div>
-					<p> morion.iot@gmail.com </p>
-				</div>
-				<div class="footer_item">
-					<div class="footer_icon_container">
-						<img src="http://wordpress-morion.rhcloud.com/wp-content/themes/zerif-lite/images/telephone65-blue.png">
-					</div>
-					<p> 3108529139 </p>
-				</div>
-
-			</div>
-		</footer>
-
+		</section>
 	</div>
 </body>
 </html>
