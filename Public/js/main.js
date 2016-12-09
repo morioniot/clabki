1c8ffb79d6d58fb74d8ce3e27ba930f8f873ae75
Nicolás Fernández <fercholas2012@gmail.com>
2016-12-09 13:41:46 -0500

    Small changes

--- Public/js/main.js
+++ Public/js/main.js
@@ -2,18 +2,18 @@ $( document ).ready(function() {
 
 	console.log("Main JS");
 
-	// tries to execute the uri:scheme
-	function uriSchemeWithHyperlinkFallback(uri, href) {
-	    if(!window.open(uri)){
-	        window.location = href;
-	    }
-	}	
+	// // tries to execute the uri:scheme
+	// function uriSchemeWithHyperlinkFallback(uri, href) {
+	//     if(!window.open(uri)){
+	//         window.location = href;
+	//     }
+	// }	
 
-	$("#facebook_link").click(function(event) {		
-	    uriSchemeWithHyperlinkFallback($(this).data('scheme'), $(this).attr('href'));
-	    // we don't want the default browser behavior kicking in and screwing everything up.
-	    event.preventDefault();
-	});
+	// $("#facebook_link").click(function(event) {		
+	//     uriSchemeWithHyperlinkFallback($(this).data('scheme'), $(this).attr('href'));
+	//     // we don't want the default browser behavior kicking in and screwing everything up.
+	//     event.preventDefault();
+	// });
 
 });
 
