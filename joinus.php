1d6bab5948dcb035e60efb062ea388e56abe7ad6
Nicolás Fernández <fercholas2012@gmail.com>
2017-01-20 18:54:00 -0500

    Changing ID of fan page to request likes count

--- joinus.php
+++ joinus.php
@@ -1,7 +1,7 @@
 <?php
     function getPageFollowers() {
 
-        $fanPageId = '795628257177459';
+        $fanPageId = '516607761870650';
         $accessToken = 'access_token=207271499718170|7ede4633f6e471307ce8a678a74922c0';
         $fields = 'fields=fan_count';
         $address = 'https://graph.facebook.com/';
